4fea4effd805bbad4f63c92244cee842803c3b5f
TetianaTitarenko <110638769+TetianaTitarenko@users.noreply.github.com>
2023-02-18 00:53:01 +0000

    add PropType

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,7 @@
+import user from 'user.json'
+import {Profile} from 'components/Profile/Profile'
+console.log('App', user)
+
 export const App = () => {
   return (
     <div
@@ -10,7 +14,7 @@ export const App = () => {
         color: '#010101'
       }}
     >
-      React homework template
+      <Profile user={user} />
     </div>
   );
 };
---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,47 @@
+import css from "components/Profile/Profile.module.css"
+// import PropTypes from 'prop-types';
+
+export const Profile = ({ user: { username, tag, location, avatar, stats: {followers, views, likes } } }) => {
+  return <div className={css.profile}>
+  <div className={css.description}>
+    <img
+      src={avatar}
+      alt={username}
+      className={css.avatar}
+    />
+    <p className={css.name}>{username}</p>
+    <p className={css.tag}>@{tag}</p>
+    <p className={css.location}>{location}</p>
+  </div>
+
+  <ul className={css.stats}>
+    <li>
+      <span className={css.label}>Followers</span>
+      <span className={css.quantity}>{followers}</span>
+    </li>
+    <li>
+      <span className={css.label}>Views</span>
+      <span className={css.quantity}>{views}</span>
+    </li>
+    <li>
+      <span className={css.label}>Likes</span>
+      <span className={css.quantity}>{likes}</span>
+    </li>
+  </ul>
+</div>
+}
+
+// Profile.propTypes = {
+//   user: PropTypes.shape({
+//     username: PropTypes.string.isRequired,
+//     tag: PropTypes.string.isRequired,
+//     location: PropTypes.number.isRequired,
+//     avatar: PropTypes.number.isRequired,
+//     stats: PropTypes.array
+//       ({
+//       followers: PropTypes.number.isRequired,
+//       views: PropTypes.number.isRequired,
+//       likes: PropTypes.number.isRequired,
+//     }).isRequired
+//   }).isRequired,
+// };
